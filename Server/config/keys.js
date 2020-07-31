b8cd423ead4c59a7954e95a7f01a45401966999c
shaggy3232 <sbrem044@uottawa.ca>
2020-07-31 14:25:39 -0400

    mongo db connection

---
+++ Server/config/keys.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+
+mongoURI =  'mongodb+srv://shaggy:shaggy@slaaydbylaay.gcce7.mongodb.net/sample_airbnb?retryWrites=true&w=majority'
+}
--- Server/index.js
+++ Server/index.js
@@ -1,18 +1,28 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require ('mongoose')
 const bodyparser = require ('body-parser')
 const nodemailer = require('nodemailer')
 const ENV = process.env.NODE_ENV;
-const PORT = process.env.PORT || 5000;
+
 require('dotenv').config();
 
+const db = require('./config/keys').mongoURI
+
+
+mongoose
+.connect(db, {
+    useNewUrlParser :true
+})
+.then( () => console.log('database connected'))
+.catch( err => console.log(err));
 
 const app = express();
 app.use (express.json());
 app.use (express.urlencoded({extended:false}));
 app.use (bodyparser.json());
 
-
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> {
     console.log(`server listening on port ${PORT}`)
